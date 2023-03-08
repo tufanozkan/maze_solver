776a1d69f59e5f93e400f9c2ed0fd535074a77f5
tufan özkan <tufanozkan1905@outlook.com>
2023-03-08 13:32:20 +0300

    GUI baslıoz

--- maze_solver_AI/src/Main.java
+++ maze_solver_AI/src/Main.java
@@ -3,7 +3,5 @@ public class Main {
     public static void main(String[] args) {
         Obstacle obstacle=new Obstacle(11);
         obstacle.generate();
-        problem2 pr= new problem2();
-        pr.
     }
 }
--- maze_solver_AI/src/Problem2GUI.java
+++ maze_solver_AI/src/Problem2GUI.java
@@ -18,17 +18,16 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
-public class Problem2GUI {
-    extends JFrame {
-        private static final long serialVersionUID = 1L;
-        private JPanel contentPane;
-        private MazePanel mazePane;
-        private JMenuBar menuBar;
-        private JMenu game;
-        private JMenu level;
-        private JMenuItem[] levels;
-        private JMenu help;
-        private JMenu computer;
+public class Problem2GUI extends JFrame {
+
+        public static final long serialVersionUID = 1L;
+        public JPanel contentPane;
+        public JMenuBar menuBar;
+        public JMenu game;
+        public JMenu level;
+        public JMenuItem[] levels;
+        public JMenu help;
+        public JMenu computer;
 
         /**
          * Launch the application.
@@ -37,7 +36,7 @@ public class Problem2GUI {
             EventQueue.invokeLater(new Runnable() {
                 public void run() {
                     try {
-                        MazeGUI frame = new MazeGUI();
+                        Problem2GUI frame = new Problem2GUI();
                         frame.setVisible(true);
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -49,119 +48,31 @@ public class Problem2GUI {
         /**
          * Create the frame.
          */
-	public MazeGUI() {
-            setSize(625, 675);
-            setResizable(false);
+	public Problem2GUI() {
+        setSize(625, 675);
+        setResizable(false);
 
-            setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            setLocationRelativeTo(null);
-            setTitle("Maze Game by Hulk");
-            contentPane = new JPanel();
-            contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-            setContentPane(contentPane);
-            contentPane.setLayout(new BorderLayout(0, 0));
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setTitle("Maze Game by Hulk");
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(new BorderLayout(0, 0));
 
-            // Menu Bar
-            menuBar = new JMenuBar();
-            setJMenuBar(menuBar);
+        // Menu Bar
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
 
-            // Menu
-            game = new JMenu("Game");
-            level = new JMenu("Level");
-            computer = new JMenu("Computer");
-            help = new JMenu("Help");
+        // Menu
+        game = new JMenu("Game");
+        level = new JMenu("Level");
+        computer = new JMenu("Computer");
+        help = new JMenu("Help");
 
-            menuBar.add(game);
-            menuBar.add(level);
-            menuBar.add(computer);
-            menuBar.add(help);
-
-            JMenuItem newGame = new JMenuItem(new AbstractAction("New Game") {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    mazePane.refresh(mazePane.getCellsMaze());
-                    repaint();
-                }
-            });
-
-            JMenuItem restart = new JMenuItem(new AbstractAction("Restart Game") {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    repaint();
-                }
-            });
-
-            JMenuItem hint = new JMenuItem(new AbstractAction("Hint") {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    mazePane.hint();
-                }
-            });
-
-            JMenuItem autoMove = new JMenuItem(new AbstractAction("Auto Move") {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    mazePane.autoMove((Graphics2D) mazePane.getGraphics());
-                }
-            });
-
-            JMenuItem algorithm = new JMenuItem(new AbstractAction("Algorithm") {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    mazePane.algorithm((Graphics2D) mazePane.getGraphics());
-                }
-            });
-
-            levels = new JMenuItem[10];
-            for(int i = 0; i < levels.length; i++) {
-                levels[i] = new JMenuItem("Level " + (i + 1));
-                levels[i].setActionCommand((i + 1) + "");
-                levels[i].addActionListener(new LevelsAction());
-                level.add(levels[i]);
-            }
-
-            JMenuItem about = new JMenuItem(new AbstractAction("About") {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                }
-            });
-
-            game.add(newGame);
-            game.add(restart);
-            game.add(hint);
-
-            computer.add(autoMove);
-            computer.add(algorithm);
-
-            help.add(about);
-
-            mazePane = new MazePanel(11);
-            contentPane.add(mazePane, BorderLayout.CENTER);
-        }
-
-        class LevelsAction extends AbstractAction{
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int number = Integer.valueOf(e.getActionCommand()) * 10 + 1;
-//			System.out.println(number);
-                mazePane.refresh(number);
-                repaint();
-            }
-
-        }
+        menuBar.add(game);
+        menuBar.add(level);
+        menuBar.add(computer);
+        menuBar.add(help);
     }
 }
